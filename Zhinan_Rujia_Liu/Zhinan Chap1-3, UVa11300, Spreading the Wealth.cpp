796dd5640be8b693688b273f00f4589262e769e5
Little-Sky <tianchicai@gmail.com>
2016-10-16 20:34:29 +0800

    Update Zhinan Chap1-3, UVa11300, Spreading the Wealth.cpp

--- Zhinan_Rujia_Liu/Zhinan Chap1-3, UVa11300, Spreading the Wealth.cpp
+++ Zhinan_Rujia_Liu/Zhinan Chap1-3, UVa11300, Spreading the Wealth.cpp
@@ -2,7 +2,11 @@
 #include <cstdlib>
 #include <algorithm>
 const int maxn = 1e6+10;
+
+/* Useful trick to simplify code & improve readability of the code
+   Use when there are too many simple loops */
 #define REP(a,b) for(i=(a); i<=(b); i++)
+
 unsigned long long a[maxn];
 long long b[maxn];
 unsigned long long sum;
@@ -19,12 +23,15 @@ int main(){
     sum /= n;
     REP (1, n-1)
       b[i] = a[i] - i*sum;
-    // printf("sum is %llu\n",sum);
     std::sort(b, b+n);  //a[kase] not included
-    // REP(1, n)
-      // printf("%lld ", b[i]);
     sum = 0;
+    
+    /* Miniminze: |a,b1| + |a,b2| + |a,b3|... 
+       To minimize the sum of distance from n points, pick the median point from them.
+       
+       Proof is simple: If not, then by moving toward to the median, more than half of the distances is decreasing.*/
     j = b[(n-1)/2];
+    
     REP(0, n-1)
       sum += std::abs(b[i]-j);
     printf("%llu\n", sum);
