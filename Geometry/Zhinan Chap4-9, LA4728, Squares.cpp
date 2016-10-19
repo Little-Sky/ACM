5cb1aae8785d5c08957ee15b6bc0878ac1d0a092
Little-Sky <tianchicai@gmail.com>
2016-10-20 01:06:04 +0800

    Update Zhinan Chap4-9, LA4728, Squares.cpp

--- Geometry/Zhinan Chap4-9, LA4728, Squares.cpp
+++ Geometry/Zhinan Chap4-9, LA4728, Squares.cpp
@@ -4,6 +4,13 @@
 #include <cmath>
 using namespace std;
 
+/*
+  An interesting question that use integers for input and output. 
+  The differences come as change all double to int. 
+  Change all comparisons from dcmp() back to common <, >, ==
+  Then, delete those division parts, and it is not a hard job.
+*/
+
 struct Point{
   int x, y;
   Point(int a=0, int b=0): x(a), y(b) { }
@@ -16,12 +23,10 @@ Vector operator - (Vector A, Vector B) { return Point(A.x-B.x, A.y-B.y); }
 
 int Cross(Vector A, Vector B) { return A.x*B.y - A.y*B.x; } //2*size of triangle
 
-//sort()
 bool operator < (const Point& a, const Point& b){
   return a.x < b.x || (a.x == b.x && a.y < b.y);
 }
 
-//cnt = unique(V, V + size) - V;
 bool operator == (const Point& a, const Point& b){
   return a.x == b.x && a.y == b.y;
 }
@@ -48,18 +53,24 @@ int Dist2(Point A, Point B) {
 }
 
 int Diameter2(Point* p, int n, Point* ch) {
-  int m = ConvexHull(p, n, ch); //m is size of ch
+  int m = ConvexHull(p, n, ch); 
   if(m == 1) return 0;
   if(m == 2) return Dist2(ch[0], ch[1]);
-  ch[m+1] = ch[0]; // 免得取模
+  ch[m+1] = ch[0];
+  
+  /* 
+     But this simplification does not affect the size of m
+     Therefore m is unchanged!
+     Debugged for about 1h... Stupid...
+  */
+  
   int ans = 0;
   for(int u = 0, v = 1; u < m; u++) {
-    // 一条直线贴住边ch[u]-ch[u+1]
     for(;;) {
       int diff = Cross(ch[u+1]-ch[u], ch[v+1]-ch[v]);
       if(diff <= 0) {
-        ans = max(ans, Dist2(ch[u], ch[v])); // u和v是对踵点
-        if(diff == 0) ans = max(ans, Dist2(ch[u], ch[v+1])); // diff == 0时u和v+1也是对踵点
+        ans = max(ans, Dist2(ch[u], ch[v])); 
+        if(diff == 0) ans = max(ans, Dist2(ch[u], ch[v+1])); 
         break;
       }
       v = (v + 1) % m;
@@ -77,6 +88,12 @@ int main(){
   while(kase--){
     scanf("%d", &n);
     for (int i=0; i<4*n; i+=4){
+      
+      /*
+        Nice try!, but then when calling Diameter(), remember to use 4*n. 
+        Or multiply the n by 4 after this input loop
+      */
+      
       p[i].read(); scanf("%d", &buf);
       p[i+1] = p[i] + Point(buf, 0);
       p[i+2] = p[i] + Point(0, buf);
